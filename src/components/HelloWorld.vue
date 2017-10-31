aa8690a66dedcdc6d66228010ff9ae33dd28cad9
wangshaoqian120 <292829389@qq.com>
2017-10-31 22:24:54 +0800

    change

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <div> {{msg}} {{age}} </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      age:18
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
